b6673bb8993c1a0fbc6fecb9909c781cbbacedfa
rizwankhan <super047240@gmail.com>
2024-07-22 12:34:51 +0530

    fronted created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,8 @@
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-
-function App() {
+const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-      </Routes>
-    </>
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello world! two</h1>
+    </div>
   )
 }
 
